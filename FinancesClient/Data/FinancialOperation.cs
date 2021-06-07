272fb3028e37c23045dcc7ebbaaf32a962198fa2
Tymofii Tarasenko <tarasenko.t.2001@gmail.com>
2021-06-08 00:33:45 +0300

    Added data model, created expenses component

---
+++ FinancesClient/Data/FinancialOperation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinancesClient.Data
+{
+    public class FinancialOperation
+    {
+        public int? Id { get; set; } = null;
+        public decimal BalanceChange { get; set; }
+        public string Type { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
