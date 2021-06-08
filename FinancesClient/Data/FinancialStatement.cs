90b1e2b4b50e5cc0308d60ee4ada8392cc842e0e
Tymofii Tarasenko <tarasenko.t.2001@gmail.com>
2021-06-08 19:31:38 +0300

    Implemented all GET methods

---
+++ FinancesClient/Data/FinancialStatement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinancesClient.Data
+{
+    public class FinancialStatement
+    {
+        public decimal TotalIncome { get; set; }
+        public decimal TotalExpense { get; set; }
+        public List<FinancialOperation> FinancialOperations { get; set; }
+    }
+}
